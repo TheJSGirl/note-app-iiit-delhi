93c1e0ef2aca93e9ebe814a05301376d4d7e848d
thejsgirl <prinegi100@gmail.com>
2019-08-24 14:04:15 +0530

    feat: added readme

---
+++ readme.md
@@ -0,0 +1,7 @@
+### Node REST Apis
+
+- It's a simple application setup
+
+### How to run Application
+- npm install
+- npm run dev
--- src/modules/notes/models.js
+++ src/modules/notes/models.js
@@ -3,10 +3,6 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const noteSchema = new Schema({
-  user: {
-    type: String,
-    trim: true,
-  },
   title: {
     type: String,
     trim: true,
